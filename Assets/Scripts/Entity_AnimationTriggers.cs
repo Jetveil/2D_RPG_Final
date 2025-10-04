0ace84879efdd30c3e0d5c60d092c78331c8e1df
pustotely <adresanetu@live.ru>
2025-10-04 15:23:53 +0300

    Basic Take damage system done

--- Assets/Scripts/Entity_AnimationTriggers.cs
+++ Assets/Scripts/Entity_AnimationTriggers.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class Entity_AnimationTriggers : MonoBehaviour
 {
     public Entity entity;
+    public Entity_Combat entityCombat;
 
     private void Awake()
     {
         entity = GetComponentInParent<Entity>();
+        entityCombat = entity.GetComponentInParent<Entity_Combat>();
     }
 
     private void CurrentStateTrigger()
     {
         entity.CallAnimationTrigger();
+    }
+
+    private void AttackTrigger()
+    {
+        entityCombat.PerformAttack();
     }
 }
