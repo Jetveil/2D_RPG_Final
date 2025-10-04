5ee14588f464d8d01ae98da42573775fdceff149
pustotely <adresanetu@live.ru>
2025-10-04 15:31:27 +0300

    Revert "Basic Take damage system done"
    This reverts commit 0ace84879efdd30c3e0d5c60d092c78331c8e1df.

--- Assets/Scripts/Entity_AnimationTriggers.cs
+++ Assets/Scripts/Entity_AnimationTriggers.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 public class Entity_AnimationTriggers : MonoBehaviour
 {
     public Entity entity;
-    public Entity_Combat entityCombat;
 
     private void Awake()
     {
         entity = GetComponentInParent<Entity>();
-        entityCombat = entity.GetComponentInParent<Entity_Combat>();
     }
 
     private void CurrentStateTrigger()
     {
         entity.CallAnimationTrigger();
-    }
-
-    private void AttackTrigger()
-    {
-        entityCombat.PerformAttack();
     }
 }
